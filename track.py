53e6a0d2c32445ca1419135a9812c32539f3f296
Zayatsoff <rozin.lior@gmail.com>
2022-07-18 00:20:58 -0400

    Changed 'artist' to 'artists'

--- track.py
+++ track.py
@@ -1,8 +1,8 @@
 class Track:
-    def __init__(self, name, id, artist):
+    def __init__(self, name, id, artists):
         self.name = name
         self.id = id
-        self.artist = artist
+        self.artists = artists
 
     def create_spotify_uri(self):
         return f"spotify:track:{self.id}"
