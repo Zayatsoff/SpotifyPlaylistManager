32313a0dfdef7ad8d4f0361a522c5ca1c06fa0d9
Zayatsoff <rozin.lior@gmail.com>
2022-07-18 00:07:44 -0400

    Create client.py

---
+++ client.py
@@ -0,0 +1,55 @@
+import json
+import requests
+from track import Track
+from playlist import Playlist
+
+
+class SpotifyClient:
+    def __init__(self, auth_token, user_id):
+        self.auth_token = auth_token
+        self.user_id = user_id
+
+    def get_last_played_trakcs(self, limit=10):
+        # Returns the last played tracks by the user
+        url = f"https://api.spotify.com/v1/me/player/recently-played?limit={limit}"
+        response = self._place_get_api_request(url)
+        response_json = response.json()
+        tracks = [
+            Track(
+                track["track"][["name"]],
+                track["track"][["id"]],
+                track["track"][["artists"]][0]["name"],
+            )
+            for track in response_json["items"]
+        ]
+        return tracks
+
+    def get_track_recommendations(self, seed_tracks, limit=50):
+        # Returns a list of recommended tracks based on one 'seed' song
+        seed_tracks_url = ""
+        for seed_track in seed_tracks:
+            seed_tracks_url += seed_track.id + ","
+        seed_tracks_url = seed_tracks_url[:-1]
+        url = f"https://api.spotify.com/v1/recommendations?seed_tracks={seed_tracks_url}&limit={limit}"
+        response = self._place_get_api_request(url)
+        response_json = response.json()
+        tracks = [
+            Track(
+                track["track"][["name"]],
+                track["track"][["id"]],
+                track["track"][["artists"]][0]["name"],
+            )
+            for track in response_json["items"]
+        ]
+        return tracks
+
+    def _place_get_api_request(self, url):
+        # Places a GET api request and returns the response
+        response = requests.get(
+            url,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": f"Bearer {self.auth_token}",
+            },
+        )
+        return response
