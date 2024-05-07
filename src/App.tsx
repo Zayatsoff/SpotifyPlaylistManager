8a05b9ea361905a60cda4d65684917c7b072afbe
Lior Rozin <93065123+Zayatsoff@users.noreply.github.com>
2024-05-07 12:32:07 -0400

    Not using github pages anymore, pivoting to other methods of hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
@@ -6,8 +5,6 @@ import Callback from "./pages/CallbackPage";
 import PlaylistsPage from "./pages/PlaylistsPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <BrowserRouter>
       <Routes>
